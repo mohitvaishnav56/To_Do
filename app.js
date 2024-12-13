53b6a39b1c37c9c9a39cb2268a5b43f48d97f3e7
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2024-12-13 20:11:52 +0530

    add Task Functionality created

--- app.js
+++ app.js
@@ -11,20 +11,28 @@ const loadTasks = ()=>{
     }
 }
 
+const saveTasks = (tasks) =>{
+    const dataJSON = JSON.stringify(tasks);
+    FS.writeFileSync(filesPath, dataJSON);
+}
+
 const addTask = (task) => {
     const tasks = loadTasks();
-    task.push(tasks);
+    tasks.push({task});
+    saveTasks(tasks);
+    console.log(`Task Added ${task}`);
+    
 }
 
 const cmd = process.argv[2];
 const arg = process.argv[3];
 
-if(cmd == add){
+if(cmd == "add"){
     addTask(arg);
 }
-else if(cmd == list){
+else if(cmd == "list"){
     listTasks();
-}else if(cmd == remove){
+}else if(cmd == "remove"){
     removeTask(arg);
 }else{
     console.log("Not Found");
