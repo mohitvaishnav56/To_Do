b37e9179b9d0214c51d39ea5fe2266e6b7d64ccd
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2024-12-13 20:25:47 +0530

    created

--- app.js
+++ app.js
@@ -1,40 +1,50 @@
-const FS = require('fs');
+const FS = require("fs");
 const filesPath = "./Tasks.json";
 
-const loadTasks = ()=>{
-    try{
-        const dataBuffer = FS.readFileSync(filesPath);
+const loadTasks = () => {
+  try {
+    const dataBuffer = FS.readFileSync(filesPath);
     const dataJSON = dataBuffer.toString();
     return JSON.parse(dataJSON);
-    }catch(error){
-        return [];
-    }
-}
+  } catch (error) {
+    return [];
+  }
+};
 
-const saveTasks = (tasks) =>{
-    const dataJSON = JSON.stringify(tasks);
-    FS.writeFileSync(filesPath, dataJSON);
-}
+const saveTasks = (tasks) => {
+  const dataJSON = JSON.stringify(tasks);
+  FS.writeFileSync(filesPath, dataJSON);
+};
 
 const addTask = (task) => {
+  const tasks = loadTasks();
+  tasks.push({ task });
+  saveTasks(tasks);
+  console.log(`Task Added ${task}`);
+};
+
+const listTasks = () => {
+  const tasks = loadTasks();
+  tasks.forEach((task, index) => {
+    console.log(`${index + 1}. ${task.task}`);
+  });
+};
+
+const removeTask = (index) => {
     const tasks = loadTasks();
-    tasks.push({task});
+    tasks.splice(index - 1, 1);
     saveTasks(tasks);
-    console.log(`Task Added ${task}`);
-    
 }
 
 const cmd = process.argv[2];
 const arg = process.argv[3];
 
-if(cmd == "add"){
-    addTask(arg);
-}
-else if(cmd == "list"){
-    listTasks();
-}else if(cmd == "remove"){
-    removeTask(arg);
-}else{
-    console.log("Not Found");
-    
+if (cmd == "add") {
+  addTask(arg);
+} else if (cmd == "list") {
+  listTasks();
+} else if (cmd == "remove") {
+  removeTask(arg);
+} else {
+  console.log("Not Found");
 }
